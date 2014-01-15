8e7679f3a689a39d1467a04fba8723de1df18fc1
Masahiro Nagano <kazeburo@gmail.com>
2014-01-15 23:44:33 +0900

    use POSIX functions and wrap tzset

--- lib/POSIX/strftime/Compiler.pm
+++ lib/POSIX/strftime/Compiler.pm
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 use Carp;
 use Time::Local qw//;
+use POSIX qw//;
 
 our $VERSION = "0.02";
 
@@ -27,6 +28,7 @@ use constant {
     TZNAME => 3,
     ISDST_CACHE => 4,
     CODE => 6,
+    TZCACHE => 7,
 };
 
 our %formats = (
@@ -34,134 +36,7 @@ our %formats = (
     'rfc822' => '%a, %d %b %y %T %z',
 );
 
-# copy from POSIX/strftime/GNU/PP.pm
-my @offset2zone = qw(
-    -11       0 SST     -11       0 SST
-    -10       0 HAST    -09       1 HADT
-    -10       0 HST     -10       0 HST
-    -09:30    0 MART    -09:30    0 MART
-    -09       0 AKST    -08       1 AKDT
-    -09       0 GAMT    -09       0 GAMT
-    -08       0 PST     -07       1 PDT
-    -08       0 PST     -08       0 PST
-    -07       0 MST     -06       1 MDT
-    -07       0 MST     -07       0 MST
-    -06       0 CST     -05       1 CDT
-    -06       0 GALT    -06       0 GALT
-    -05       0 ECT     -05       0 ECT
-    -05       0 EST     -04       1 EDT
-    -05       1 EASST   -06       0 EAST
-    -04:30    0 VET     -04:30    0 VET
-    -04       0 AMT     -04       0 AMT
-    -04       0 AST     -03       1 ADT
-    -03:30    0 NST     -02:30    1 NDT
-    -03       0 ART     -03       0 ART
-    -03       0 PMST    -02       1 PMDT
-    -03       1 AMST    -04       0 AMT
-    -03       1 WARST   -03       1 WARST
-    -02       0 FNT     -02       0 FNT
-    -02       1 UYST    -03       0 UYT
-    -01       0 AZOT    +00       1 AZOST
-    -01       0 CVT     -01       0 CVT
-    +00       0 GMT     +00       0 GMT
-    +00       0 WET     +01       1 WEST
-    +01       0 CET     +02       1 CEST
-    +01       0 WAT     +01       0 WAT
-    +02       0 EET     +02       0 EET
-    +02       0 IST     +03       1 IDT
-    +02       1 WAST    +01       0 WAT
-    +03       0 FET     +03       0 FET
-    +03:07:04 0 zzz     +03:07:04 0 zzz
-    +03:30    0 IRST    +04:30    1 IRDT
-    +04       0 AZT     +05       1 AZST
-    +04       0 GST     +04       0 GST
-    +04:30    0 AFT     +04:30    0 AFT
-    +05       0 DAVT    +07       0 DAVT
-    +05       0 MVT     +05       0 MVT
-    +05:30    0 IST     +05:30    0 IST
-    +05:45    0 NPT     +05:45    0 NPT
-    +06       0 BDT     +06       0 BDT
-    +06:30    0 CCT     +06:30    0 CCT
-    +07       0 ICT     +07       0 ICT
-    +08       0 HKT     +08       0 HKT
-    +08:45    0 CWST    +08:45    0 CWST
-    +09       0 JST     +09       0 JST
-    +09:30    0 CST     +09:30    0 CST
-    +10       0 PGT     +10       0 PGT
-    +10:30    1 CST     +09:30    0 CST
-    +11       0 CAST    +08       0 WST
-    +11       0 NCT     +11       0 NCT
-    +11       1 EST     +10       0 EST
-    +11       1 LHST    +10:30    0 LHST
-    +11:30    0 NFT     +11:30    0 NFT
-    +12       0 FJT     +12       0 FJT
-    +13       0 TKT     +13       0 TKT
-    +13       1 NZDT    +12       0 NZST
-    +13:45    1 CHADT   +12:45    0 CHAST
-    +14       0 LINT    +14       0 LINT
-    +14       1 WSDT    +13       0 WST
-);
-
-sub tzoffset {
-    my ($colons, @t) = @_;
-
-    # Normalize @t array, we need seconds without frac
-    $t[0] = int $t[0];
-
-    my $diff = (exists $ENV{TZ} and $ENV{TZ} eq 'GMT')
-             ? 0
-             : Time::Local::timegm(@t) - Time::Local::timelocal(@t);
-    my $h = $diff / 60 / 60;
-    my $m = $diff / 60 % 60;
-    my $s = $diff % 60;
-
-    my $fmt = do {
-        if ($colons == 0) {
-            '%+03d%02u';
-        }
-        elsif ($colons == 1) {
-            '%+03d:%02u';
-        }
-        elsif ($colons == 2) {
-            '%+03d:%02u:%02u';
-        }
-        elsif ($colons == 3) {
-            $s ? '%+03d:%02u:%02u' : $m ? '%+03d:%02u' : '%+03d';
-        }
-        else {
-            '%%' . ':' x $colons . 'z';
-        };
-    };
-
-    return sprintf $fmt, $h, $m, $s;
-}
-
-sub tzname {
-    my @t = @_;
-
-    return 'GMT' if exists $ENV{TZ} and $ENV{TZ} eq 'GMT';
-
-    my $diff = tzoffset(3, @t);
-
-    my @t1 = my @t2 = @t;
-    @t1[3,4] = (1, 1);  # winter
-    @t2[3,4] = (1, 7);  # summer
-
-    my $diff1 = tzoffset(3, @t1);
-    my $diff2 = tzoffset(3, @t2);
-
-    for (my $i=0; $i < @offset2zone; $i += 6) {
-        next unless $offset2zone[$i] eq $diff1 and $offset2zone[$i+3] eq $diff2;
-        return $diff2 eq $diff ? $offset2zone[$i+5] : $offset2zone[$i+2];
-    }
-
-    if ($diff =~ /^([+-])(\d\d)$/) {
-        return sprintf 'GMT%s%d', $1 eq '-' ? '+' : '-', $2;
-    };
-
-    return 'Etc';
-}
-
+# copy from POSIX/strftime/GNU/PP.pm and modify
 sub iso_week_days {
     my ($yday, $wday) = @_;
 
@@ -284,6 +159,9 @@ sub new {
 }
 
 
+my $INSTALL_TZSET = 0;
+my $CLEAR_TZCACHE = 0;
+
 sub compile {
     my $self = shift;
     my $fmt = $self->[FMT];
@@ -301,18 +179,38 @@ sub compile {
     my @weekday_abbr = qw( Sun Mon Tue Wed Thu Fri Sat );
     my @month_name = qw( January February March April May June July August September October November December );
     my @month_abbr = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
+
+    if ( !$INSTALL_TZSET ) {
+        my $orignal = POSIX->can('tzset');
+        {
+            no warnings 'redefine';
+            *POSIX::tzset = sub {
+                $orignal->();
+                $CLEAR_TZCACHE++;
+            };
+        }
+        $INSTALL_TZSET = 1;
+    }
+    $self->[TZCACHE] = $CLEAR_TZCACHE;
+
     $fmt = q~sub ($@) {
         my $this = shift;
         if ( @_ != 9  && @_ != 6 ) {
             Carp::croak 'Usage: to_string(sec, min, hour, mday, mon, year, wday = -1, yday = -1, isdst = -1)';
         }
         if ( @_ == 6 ) {
-            @_ = gmtime(Time::Local::timegm(@_));
+            @_ = localtime(Time::Local::timelocal(@_));
         }
-        if ( ! defined $this->[TZOFFSET] || ! defined $this->[ISDST_CACHE] || $_[ISDST] ne $this->[ISDST_CACHE] ) {
+        if (   $CLEAR_TZCACHE > $self->[TZCACHE] 
+            || ! defined $this->[TZOFFSET] 
+            || ! defined $this->[ISDST_CACHE] 
+            || $_[ISDST] ne $this->[ISDST_CACHE] )
+        {
+            scalar localtime(); # for init
+            my $min_offset = int((Time::Local::timegm(@_) - Time::Local::timelocal(@_)) / 60);
+            $this->[TZOFFSET] = sprintf '%+03d%02u', $min_offset / 60, $min_offset % 60;
+            $this->[TZNAME] = (POSIX::tzname())[$_[ISDST]];
             $this->[ISDST_CACHE] = $_[ISDST];
-            $this->[TZOFFSET] = tzoffset(0, @_);
-            $this->[TZNAME] = tzname(@_);
         }
         sprintf(q!~ . $fmt .  q~!,~ . join(",", @$args) . q~);
     }~;
